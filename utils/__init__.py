5d571fd5ca213d8084f38e6eeda458819176721f
amirnak <amirnakgamer@gmail.com>
2024-11-24 19:32:36 +0300

    Inittial commit

---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from .put_projects_href import get_projects_href
---
+++ utils/put_projects_href.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+
+
+def get_projects_href(html):
+    soup = BeautifulSoup(html, "html.parser")
+    projects = soup.find(
+        "div", class_="project-list__items project-list__items--column project-list__items--type-projects-page")
+    projects_a = projects.findChildren("a", recursive=False)
+    projects_href = [project["href"] for project in projects_a]
+    projects_href = [
+        project for project in projects_href if not project.startswith("https://vk.com/")]
+    return projects_href
